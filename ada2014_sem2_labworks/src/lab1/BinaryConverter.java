d94aa04d62b0f98eb92ff9509de59dca496a6f74
yue <makingitbettergo@gmail.com>
2014-07-22 23:44:04 +1200

    rearranged test order for generate spreadsheet document.. program efficiency seems O(n).

--- ada2014_sem2_labworks/src/lab1/BinaryConverter.java
+++ ada2014_sem2_labworks/src/lab1/BinaryConverter.java
@@ -49,7 +49,8 @@ public class BinaryConverter {
 	}
 
 	public static void main(String[] args) {
-		int[] decimals = { 12, 3, 128, 15, 1, 195742 };
+		int[] decimals = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048,
+				4096, 8192, 16384, 32768, 65536, 131072, 262144 };
 		BinaryConverter bc = new BinaryConverter(decimals);
 		bc.convertToBinary();
 	}
