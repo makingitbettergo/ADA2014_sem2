8f2de8ce0b3e9d95b73e463b62dc3687c6f948b8
yue <makingitbettergo@gmail.com>
2014-07-24 00:02:10 +1200

    completed Fibonacci class. Found the current algorithm grows exponentially.

--- ada2014_sem2_labworks/src/lab1/FibonacciNumber.java
+++ ada2014_sem2_labworks/src/lab1/FibonacciNumber.java
@@ -1,6 +1,24 @@
 package lab1;
 
 public class FibonacciNumber {
-	
-	
+
+	public int fib(int n) {
+		if (n < 2)
+			return 1;
+		else
+			return fib(n - 2) + fib(n - 1);
+	}
+
+	public static void main(String[] args) {
+		FibonacciNumber fn = new FibonacciNumber();
+		long[] fibonaccis = new long[40];
+
+		for (int i = 0; i < fibonaccis.length; i++) {
+			fibonaccis[i] = fn.fib(i);
+		}
+
+		for (long l : fibonaccis) {
+			System.out.println(l);
+		}
+	}
 }
